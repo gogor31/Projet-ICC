15bcefb958cb81167818f44fc9433d386b2c4eec
swag <117921961+swaggbln@users.noreply.github.com>
2026-04-07 19:58:32 +0200

    implémentation de l'architecture
    modificatons du makefile, certains changement sont pt sepicifiques a des problèmes macOS, ancienne version dans le dossier backups,
    [project.cc]: tentative (ratée) d'afficher une fenêtre

--- BrickBreakerTest/project.cc
+++ BrickBreakerTest/project.cc
@@ -27,20 +27,21 @@ int main(int argc, char* argv[]) {
 */
 
 int main(int argc, char* argv[]) {
-    Game game;
+    auto app = Gtk::Application::create("org.gtkmm.brickbreaker");
 
+    Game game;
+    My_window window(argc == 2 ? argv[1] : "");
+        
     //lancement du jeu sans fichier test, ->[?] avec fichier init_state ou une instance game init_state
     if (argc == 1) {
-        return 0;
+        return app->run(window);
     }
 
     //lancement du jeu avec fichier test, comme rendu1
     if  (argc == 2){
         // Tentative de chargement et de validation du fichier d'entrée
-        if (!game.load_file(argv[1])) {return 1;}
-        return 0;
+        if (!game.load_file(argv[1])) {return app->run(window);}
     }
     //les autres valeurs possibles pour argc sont invalides
     return 1;
-
 }
