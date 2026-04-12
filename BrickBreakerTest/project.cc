7a6d32dd2200f02e55ee90bbb9780b22d1b442d4
swag <117921961+swaggbln@users.noreply.github.com>
2026-04-12 15:52:51 +0200

    ouverture de fenêtre sans fichier test (progrès)
    [project.cc]: ouverture de fenêtre sans fichier test avec succès, les boutons et la détection de la souris sont fonctionnels (il faut vérifier encore que ça fonctionne sur la VM)

--- BrickBreakerTest/project.cc
+++ BrickBreakerTest/project.cc
@@ -8,40 +8,21 @@
 #include "gui.h"
 
 
-/* [V1]
 int main(int argc, char* argv[]) {
-    // Vérification de la présence du fichier de configuration en argument
-    if (argc != 2) {
-        return 1;
-    }
-
-    // Utilisation du namespace pour instancier le moteur de jeu (O31.2)
-    Game game;
-
-    // Tentative de chargement et de validation du fichier d'entrée
-    if (!(game.load_file(argv[1]))) {
-    return 1;}
-
-    return 0;
-}
-*/
-
-int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.gtkmm.brickbreaker");
+    auto app = Gtk::Application::create();
 
     Game game;
-    My_window window(argc == 2 ? argv[1] : "");
-        
-    //lancement du jeu sans fichier test, ->[?] avec fichier init_state ou une instance game init_state
+    
+    //lancement du jeu sans fichier test
     if (argc == 1) {
-        return app->run(window);
+        return app->make_window_and_run<My_window>(argc, argv, "temp");
     }
 
     //lancement du jeu avec fichier test, comme rendu1
-    if  (argc == 2){
-        // Tentative de chargement et de validation du fichier d'entrée
-        if (!game.load_file(argv[1])) {return app->run(window);}
+    if (argc == 2) { 
+        if (!game.load_file(argv[1])) {
+            return app->make_window_and_run<My_window>(argc, argv, "temp");
+        }
     }
-    //les autres valeurs possibles pour argc sont invalides
     return 1;
 }
