e7e66d6648d53446d367387aff14027509048e33
Ilhan <ilhanlegio@gmail.com>
2026-04-13 22:58:22 +0200

    Travail sur graphic (.h et .cc)

--- BrickBreakerTest/graphic.h
+++ BrickBreakerTest/graphic.h
@@ -1,17 +1,15 @@
 #ifndef GRAPHIC_H
 #define GRAPHIC_H
 
-enum Color
-{
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    CYAN,
-    BLUE,
-    PURPLE,
-    BLACK,
-    GREY,
-    WHITE,
-};
+namespace graphic {
+
+enum Color { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, BLACK, GREY, WHITE };
+
+void draw_arena();
+void draw_square(double x, double y, double size, Color color);
+void draw_circle(double x, double y, double radius, Color color, bool filled = true);
+void draw_arc(double x, double y, double radius, Color color);
+
+}
+
 #endif
