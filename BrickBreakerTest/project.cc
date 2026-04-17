d6d6b55f83185bd3163ad3db552fcef0de1cf961
swag <117921961+swaggbln@users.noreply.github.com>
2026-04-17 18:35:58 +0200

    affichage fonctionnel
    l'affichage est fonctionnel et (visiblement) complet

--- BrickBreakerTest/project.cc
+++ BrickBreakerTest/project.cc
@@ -7,24 +7,7 @@ int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create("org.gtkmm.brickbreaker");
     
     app->set_flags(Gio::Application::Flags::HANDLES_OPEN);
-
-    //Game game;
     std::string filename = (argc == 2) ? argv[1] : "";
-    return app->make_window_and_run<My_window>(argc, argv, filename);
 
-    /*
-    //lancement du jeu sans fichier test
-    if (argc == 1) {
-        return app->make_window_and_run<My_window>(argc, argv, "no_file");
-    }
-
-    //lancement du jeu avec fichier test, comme rendu1
-    if (argc == 2) { 
-        if (!game.load_file(argv[1])) {
-            return 1;  // Echec de chargement du fichier
-        } else {
-            return app->make_window_and_run<My_window>(argc, argv, filename);
-        }
-    }
-        */
+    return app->make_window_and_run<My_window>(1, argv, filename);
 }
